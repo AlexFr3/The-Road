c44a83fcd302ba23e5d7ab1892f7f8b2b29268c5
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-05-16 19:01:45 +0200

    added initialize function to bellman-ford algorithm

--- 0001089191.c
+++ 0001089191.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <float.h>
+
 typedef struct Edge {
     int src;            /* nodo sorgente        */
     int dst;            /* nodo destinazione    */
@@ -23,6 +25,63 @@ typedef struct {
     int m;              
     int mat[ROWS][COLS];
 } Matrix;
+
+void initialize(const Graph *g, int s, double *d, int *p) {
+    int i;
+    for (i = 0; i < g->n; i++) {
+        d[i] = DBL_MAX; /* Imposto tutte le distanze a "infinito teorico" (valore massimo per gli interi)*/
+        p[i] = -1;       /*Imposto tutti predecessori non definiti ovvero -1*/
+    }
+    d[s] = 0; /* Imposto la distanza del nodo sorgente a 0*/
+}
+
+int bellman_ford( const Graph *g, int s, double *d, int *p, const Edge **sp )
+{ 
+  initialize(g,s,d,p);/*s punto di partenza da 0, G è il grafo*/
+  return 0;
+}
+void graph_destroy(Graph *g)
+{
+    int i;
+    assert(g != NULL);
+
+    for (i=0; i<g->n; i++) {
+        Edge *edge = g->edges[i];
+        while (edge != NULL) {
+            Edge *next = edge->next;
+            free(edge);
+            edge = next;
+        }
+        g->edges[i] = NULL; /* e' superfluo */
+    }
+    free(g->edges);
+    free(g->in_deg);
+    free(g->out_deg);
+    g->n = 0;
+    g->edges = NULL;
+    free(g);
+}
+Graph *graph_create( int n)
+{
+    int i;
+    Graph *g = (Graph*)malloc(sizeof(*g));
+    assert(g != NULL);
+    assert(n > 0);
+
+    g->n = n;
+    g->m = 0;
+    g->edges = (Edge**)malloc(n * sizeof(Edge*));
+    assert(g->edges != NULL);
+    g->in_deg = (int*)malloc(n * sizeof(*(g->in_deg)));
+    assert(g->in_deg != NULL);
+    g->out_deg = (int*)malloc(n * sizeof(*(g->out_deg)));
+    assert(g->out_deg != NULL);
+    for (i=0; i<n; i++) {
+        g->edges[i] = NULL;
+        g->in_deg[i] = g->out_deg[i] = 0;
+    }
+    return g;
+}
 
 void print_matrix(Matrix* mat) {
     int i, j;
@@ -83,13 +142,21 @@ Graph *graph_read_from_file(FILE *f)
     matrix->m = m;  
     matrix=read_matrix_from_file(f,n,m);
     print_matrix(matrix);
+    g= graph_create(n);
     return g;
 }
 int main( int argc, char *argv[] )
 {
     Graph *G;
     FILE *filein = stdin;
-
+  const Edge **sp; /* sp[v] è il puntatore all'arco nel grafo che
+                        collega v con il suo predecessore nell'albero
+                        dei cammini minimi */
+    double *d;       /* d[v] è la distanza del nodo v dalla
+                        sorgente */
+    int *p;          /* p[v] è il predecessore di v lungo il cammino
+                        minimo dalla sorgente a v */
+    int neg, src = 0;
     if ( (argc < 2) || (argc > 4) ) {
         fprintf(stderr, "Usage: %s filename [src [dst]]\n", argv[0]);
         return EXIT_FAILURE;
@@ -102,6 +169,13 @@ int main( int argc, char *argv[] )
         }
     }
     G = graph_read_from_file(filein);/*creo la matrice dal file*/
+     d = (double*)malloc(G->n * sizeof(*d)); assert(d != NULL);
+    p = (int*)malloc(G->n  * sizeof(*p)); assert(p != NULL);
+    sp = (const Edge**)malloc(G->n  * sizeof(*sp)); assert(sp != NULL);
+
+    neg = bellman_ford(G, src, d, p, sp);
+ 
+    graph_destroy(G);
     if (filein != stdin) fclose(filein);
     return EXIT_SUCCESS;
 }
